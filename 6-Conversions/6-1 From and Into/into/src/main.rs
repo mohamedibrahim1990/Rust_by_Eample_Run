ecf42546b1407f1291f308a0c77bf2a6aeb6cbde
mohamedibrahim51990 <mohamedibrahim51990@gmail.com>
2021-02-05 09:56:18 +0200

    6-conversions done run

---
+++ 6-Conversions/6-1 From and Into/into/src/main.rs
@@ -0,0 +1,42 @@
+/*fn main() {
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+}*/
+
+/*use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}*/
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let int = 5;
+    // Try removing the type declaration
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
